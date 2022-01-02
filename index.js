dbcd51c4086b3e6553e27ede5e1fbb3f278018e2
Carlos Umaña Acevedo <carumtito@gmail.com>
2022-01-02 03:17:36 -0600

    Swagger documentation

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const port = 3000;
 
+const swaggerUi = require("swagger-ui-express");
+const specs = require("./lib/swagger");
+
 const routes = require("./lib/routes");
 
-require("dotenv").config();
-
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 app.use(express.json());
-
 app.use(routes);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
