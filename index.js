75118420a01d2a412c8fbb3de714f9671a93b40b
Carlos Umaña Acevedo <carumtito@gmail.com>
2022-01-02 00:12:05 -0600

    Move routes

--- index.js
+++ index.js
@@ -2,35 +2,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const routes = require("./lib/routes");
+
 require("dotenv").config();
 
 app.use(express.json());
 
-const songs = require("./lib/songs");
-const search = require("./lib/search");
-const lyrics = require("./lib/lyrics");
-
-app.get("/", (req, res) => res.send("Hello World!, Carlos"));
-
-app.get("/search", async (req, res) => {
-  const { q } = req.query;
-  const response = await search(q);
-
-  res.json(response);
-});
-
-app.get("/songs/:source/:id", async (req, res) => {
-  const { source, id } = req.params;
-  const response = await songs(source, id);
-
-  res.send(response);
-});
-
-app.get("/lyrics/:artist/:title", async (req, res) => {
-  const { artist, title } = req.params;
-  const response = await lyrics(artist, title);
-
-  res.send(response);
-});
+app.use(routes);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
